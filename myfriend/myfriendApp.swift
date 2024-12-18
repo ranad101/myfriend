62eed29b61c28c156744a5eb004a1c081b1b6c96
ranad101 <aldawoodranad@outlook.com>
2024-12-18 15:45:16 +0300

    Initial Commit

---
+++ myfriend/myfriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfriendApp.swift
+//  myfriend
+//
+//  Created by Ranad aldawood on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct myfriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myfriendTests/myfriendTests.swift
@@ -0,0 +1,17 @@
+//
+//  myfriendTests.swift
+//  myfriendTests
+//
+//  Created by Ranad aldawood on 17/06/1446 AH.
+//
+
+import Testing
+@testable import myfriend
+
+struct myfriendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
